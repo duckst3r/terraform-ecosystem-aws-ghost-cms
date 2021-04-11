a4ed8536d83a686606417a90adc1d02c9a2c8ce5
Charles CEBE <cebecharles@gmail.com>
2021-04-10 22:12:17 -0400

    fix: move provider config in provider.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
-provider "aws" {
-  region     = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
+#####
+# Networking Module
+#####
 
 module "networking" {
   source = "./networking"
